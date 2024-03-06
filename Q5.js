3ab580c00aeb44ec17922d0c3cbd5fae3791b2ea
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-06 21:26:49 +0500

    Add files via upload
    It is the Fifth task of the assignment.

---
+++ Q5.js
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Theodore Roosevelt";
+var message = "Believe you can and you're halfway there";
+console.log("".concat(famous_person, "once said,\"").concat(message));
